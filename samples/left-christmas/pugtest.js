0a02ca6e5237fd31850d8521cfb7b1f9d80dff21
Justin Schwartzbeck <justinmschw@gmail.com>
2021-12-20 13:51:01 -0600

    Adds Christmas theme

---
+++ samples/left-christmas/pugtest.js
@@ -0,0 +1,26 @@
+const LeftPage = require('../..//lib/left-page');
+
+const myLeftPage = new LeftPage('christmas', 'Sample Page', {
+  pugFile: './content-home.pug',
+  gen: function() {
+    return Promise.resolve({});
+  }
+});
+
+myLeftPage.setHeader('./header.pug');
+
+myLeftPage.addPage({
+  path: '/page1',
+  pugFile: './content-page1.pug',
+  gen: async function() {
+    return Promise.resolve({name: 'Justin'});
+  },
+  title: 'Page 1'
+});
+
+myLeftPage.addLink({
+  url: '/page1',
+  text: 'Page 1'
+});
+
+myLeftPage.start();
