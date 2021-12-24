1bc0d9edd9f5fec357483d129a048cc740a3dffc
Justin Schwartzbeck <justinmschw@gmail.com>
2021-12-24 05:42:07 -0600

    Adds spring and ocean themes

---
+++ samples/left-ocean/ocean.js
@@ -0,0 +1,30 @@
+const Page = require('../../lib/page');
+const pug = require('pug');
+const fs = require('fs');
+
+const header = fs.readFileSync('./header.html', 'utf-8');
+const contentHome = pug.render(fs.readFileSync('./content-home.pug', 'utf-8'), {});
+const contentPage1 = fs.readFileSync('./content-page1.pug', 'utf-8');
+
+const myLeftPage = new Page('leftpane', 'ocean', 'Sample Page', {
+  gen: function() {
+    return Promise.resolve(contentHome);
+  }
+});
+
+myLeftPage.setHeader(header);
+
+myLeftPage.addPage({
+  path: '/page1',
+  gen: async function() {
+    return Promise.resolve(pug.render(contentPage1, {name: 'Justin'}));
+  },
+  title: 'Page 1'
+});
+
+myLeftPage.addLink({
+  url: '/page1',
+  text: 'Page 1'
+});
+
+myLeftPage.start();
